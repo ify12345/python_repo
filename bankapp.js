d976d26b87104832b4f9a8b0a087afecddd05424
IFEANYI UCHE <ifeanyiuchenwakodo@gmail.com>
2023-06-01 23:10:24 +0100

    2

---
+++ bankapp.js
@@ -0,0 +1,26 @@
+anime({
+    targets: 'h1',
+    translateY: [-50, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: 'easeInOutQuad'
+  });
+  
+  anime({
+    targets: 'p',
+    translateY: [-50, 0],
+    opacity: [0, 1],
+    delay: 200,
+    duration: 1000,
+    easing: 'easeInOutQuad'
+  });
+  
+  anime({
+    targets: '.btn',
+    translateY: [50, 0],
+    opacity: [0, 1],
+    delay: 400,
+    duration: 1000,
+    easing: 'easeInOutQuad'
+  });
+  
